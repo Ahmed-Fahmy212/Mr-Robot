cc6e3b7e507cc588cba9abde31bec70c8768cbfa
Ahmed-Fahmy212 <126887643+Ahmed-Fahmy212@users.noreply.github.com>
2024-08-20 19:58:20 +0300

    initialize the app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// import {
+//   createBrowserRouter,
+//   Route,
+//   createRoutesfromElements,
+//   RouterProvider,
+// } from "react-router-dom";
+// const router = createBrowserRouter(
+//   createRoutesfromElements(<Route index element={<h1>My APP</h1>} />)
+// );
+// const App = () => {
+//   return <RouterProvider router={router} />;
+// };
+
+// export default App;
