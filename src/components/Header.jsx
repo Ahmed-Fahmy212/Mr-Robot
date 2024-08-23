e5858bc70f55669b82af757444e85204f98db468
Ahmed-Fahmy212 <126887643+Ahmed-Fahmy212@users.noreply.github.com>
2024-08-23 23:49:32 +0300

    some edit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,17 +6,20 @@ function Header() {
 
   return (
     <div
-      className={`fixed top-0 left-0 w-full z-50  border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm bg-n-8/90 backdrop-blur-sm         `}
-    >
+      className={`fixed top-0 left-0 w-full z-50 
+        border-b border-n-6 
+        bg-n-8/90 backdrop-blur-sm
+        lg:bg-n-8/90  --- lg:backdrop-blur-sm
+        `}>
+    
       <div className="flex items-center px-5 lg:px-7.5 max-lg:py-4 xl:px-10">
         <a className="block w-[12rem] xl:mr-8" href="#hero">
           <img src={brainwave} width={190} height={40} alt="BrainWave" />
         </a>
 
-
-         {/* /* ----------------------------- why this fixed ----------------------------- */ }
+        {/* /* ----------------------------- why this fixed ----------------------------- */}
         <nav className=" fixed top[5-rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent">
-           {/* -------------------------------- mid words ------------------------------- */ }
+          {/* -------------------------------- mid words ------------------------------- */}
           <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
             {navigation.map((elem) => (
               <a
@@ -31,9 +34,7 @@ function Header() {
             ))}
           </div>
         </nav>
-         
 
-         
         {/* want add the bottom here  */}
       </div>
     </div>
