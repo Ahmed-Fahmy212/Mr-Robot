2be301c85110fc8316fe38e3db772c66b2d8a754
Ahmed-Fahmy212 <126887643+Ahmed-Fahmy212@users.noreply.github.com>
2024-08-31 15:46:34 +0300

    fake data untill fetch added

---
+++ src/exData/data.js
@@ -0,0 +1,32 @@
+export const navbarMenu = [
+    {
+        id: 1,
+        title: "Home",
+        url: "#",
+        onlyMobile: false,
+    },
+    {
+        id: 2,
+        title: "Trainer",
+        url: "#",
+        onlyMobile: false,
+    },
+    {
+        id: 3,
+        title: "Programs",
+        url: "#",
+        onlyMobile: false,
+    },
+    {
+        id: 4,
+        title: "Blogs",
+        url: "#",
+        onlyMobile: false,
+    },
+    {
+        id: 5,
+        title: "Pricing",
+        url: "#",
+        onlyMobile: false,
+    },
+    ];
