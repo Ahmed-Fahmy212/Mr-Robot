82bb370f2ee940a41d374b29953a30d6f08151d4
Ahmed-Fahmy212 <126887643+Ahmed-Fahmy212@users.noreply.github.com>
2024-08-31 15:46:51 +0300

    some data added in hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -10,12 +10,13 @@ const Hero = () => {
         <div className="flex flex-col justify-center py-14 md:py-0 font-playfair">
           
           {/* Heading and subheading section */}
-          <div className="text-center md:text-left space-y-6 mb-8">
-            <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold leading-relaxed xl:leading-normal">
+          <div className="text-center md:text-left space-y-6 mb-8 ">
+            <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold leading-relaxed xl:leading-normal "
+            style={{filter:"drop-shadow(2px 2px 4px rgba(0, 0, 0, 0.1))"}}>
               Transform your body, transform your{" "}
-              <span className="text-c1 font-bold">Life</span>
+              <span className="text-c1 font-bold ">Life</span>
             </h1>
-            <p className="text-gray-600 xl:max-w-[500px]">
+            <p className="text-gray-600 md:text-xl xl:max-w-[600px]">
               Join us today and start your fitness journey.
             </p>
           </div>
@@ -33,14 +34,15 @@ const Hero = () => {
           
         </div>
         
-        {/* right side content*/}
-        <div className="hidden md:flex items-center justify-center">
+        {/* right side content in Hero sectiojn*/}
+        <div className="hidden md:flex items-center justify-center relative">
           <img
             src={heroImg}
             alt=""
-            className="w-[350px] md:wd-[450px] lg:w-[550px] xl:w-[650px]  "
+            className="w-[350px] md:wd-[450px] lg:w-[550px] xl:w-[650px] absolute inset-0 "
+            style={{ filter: "drop-shadow(2px 2px 4px rgba(0, 0, 0, 0.7))" }}
           />
-      </div>
+        </div>
       </div>
     </>
   );
